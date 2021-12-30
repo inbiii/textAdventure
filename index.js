831d795096e78e68f9da352919b2d81062475642
inbiii <omaidksquared@gmail.com>
2021-12-30 11:57:49 -0500

    second commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3040;
 app.set("view engine", 'ejs');
-
+app.use(express.static(__dirname + '/public'));
 ////////////////////////////////////////////Routes
 app.get('/', (req, res) => {
     res.render('title')
